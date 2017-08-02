3563d7c2017b6c687444de848248cc3e758b9efa
Peter Wang <happypeter1983@gmail.com>
2017-08-02 10:32:47 +0800

    set img

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import axios from 'axios'
 
 class App extends Component {
 
+  state = {
+    imgUrl: ''
+  }
+
   handleChange = (event) => {
     const file = event.target.files[0]
     const reader = new FileReader()
@@ -17,12 +21,17 @@ class App extends Component {
       //  数据挂到 form 上才能发送
       let formData = new FormData()
       formData.append('avatar', file)
-      // 这样，可以保证 multipart/form-data 
+      // 这样，可以保证 multipart/form-data
 
 
-      axios.post(`http://localhost:3008/upload/avater`, formData).then(
+      axios.post(`http://192.168.0.119:3008/touxiang`, formData).then(
         res=> {
-
+          console.log('ppp...', res.data)
+          let fullImgUrl = `http://192.168.0.119:3008/uploads/avatars/${res.data.filename}`
+          console.log('文件路径：', fullImgUrl)
+          this.setState({
+            imgUrl: fullImgUrl
+          })
         }
       )
       // 只有在这里，执行 file 相关操作，那么 file 里面才是有真正数据的
@@ -33,6 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={this.state.imgUrl} alt="头像" />
         <input type="file" onChange={this.handleChange}
           className="file-upload-input" />
       </div>
